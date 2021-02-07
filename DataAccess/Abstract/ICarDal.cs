2183d324d1f73655a90ba55bb8f55cb05683d020
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-02-07 22:38:57 +0300

    📝Add xml document for ICarDal

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
+    /// <summary>
+    /// This interface includes custom query for Car Entity
+    /// </summary>
     public interface ICarDal
     {
         List<Car> GetAll();
