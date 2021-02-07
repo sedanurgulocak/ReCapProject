ca4f40ab561f4173d077a74316d8565a2aec96e4
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-07 23:06:26 +0300

    📝 Add XML docuemnt for methods in ICarDal interface.

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -10,10 +10,47 @@ namespace DataAccess.Abstract
     /// </summary>
     public interface ICarDal
     {
+        /// <summary>
+        /// Get All Cars
+        /// </summary>
+        /// <returns>
+        /// List of Car
+        /// </returns>
         List<Car> GetAll();
-        List<Car> GetById(int carId);
+
+        /// <summary>
+        /// Get Cars with CarId
+        /// </summary>
+        /// <param name="carId">
+        /// PK of Car Entity
+        /// </param>
+        /// <returns>
+        /// Car Entity
+        /// </returns>
+        Car GetById(int carId);
+
+        /// <summary>
+        /// Add new car
+        /// </summary>
+        /// <param name="car">
+        /// Car Entity
+        /// </param>
         void Add(Car car);
+
+        /// <summary>
+        /// Update Car
+        /// </summary>
+        /// <param name="car">
+        /// Car Entity
+        /// </param>
         void Update(Car car);
+
+        /// <summary>
+        /// Delete Car
+        /// </summary>
+        /// <param name="car">
+        /// Car Entity
+        /// </param>
         void Delete(Car car);
 
     }
