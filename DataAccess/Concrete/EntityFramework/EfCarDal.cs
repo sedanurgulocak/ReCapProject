dd005eeb4b7e607d716ef45a270c1c6870c3f47d
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-08 23:38:38 +0300

    ✨ Interface implementation for EfCarDal

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal
+    public class EfCarDal : ICarDal
     {
+        public void Add(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetById(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
