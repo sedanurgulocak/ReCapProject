3d842877f4771a708f5085d9ea8061f20dd76d11
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-08 23:30:04 +0300

    ✨ Interface implementation for EfBrandDal

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfBrandDal
+    public class EfBrandDal : IBrandDal
     {
+        public void Add(Brand entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Brand entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetById(Expression<Func<Brand, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Brand entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
