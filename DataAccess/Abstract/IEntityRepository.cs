50d5298c7b1161a4df2daaace7a6d625af5de4b6
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-08 23:00:13 +0300

    ✨ Add new methods for Entity Repository interface

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository
-    {
+    public interface IEntityRepository<T>
+    { 
+        List<T> GetAll();
+        T GetById(int entity);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
     }
 }
