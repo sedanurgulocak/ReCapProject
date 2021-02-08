62b628ecf022673169e250d44a84983779bb67cc
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-09 00:36:42 +0300

    ✨ Implement interface IEntity

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    public interface IEntityRepository<T> where T:class, IEntity, new()
     { 
         List<T> GetAll(Expression<Func<T, bool>> filter=null);
         T GetById(Expression<Func<T, bool>> filter);
