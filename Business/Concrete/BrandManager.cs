ae5e10ca7801ce155443dd1cbcc7fea157f74ea8
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-10 00:50:07 +0300

    ✨ Add Brand Manager class

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public List<Brand> GetAll()
+        {
+            //Business Code
+            return _brandDal.GetAll();
+        }
+
+        public Brand GetById(int brandId)
+        {
+            return _brandDal.GetById(b => b.BrandId == brandId);
+        }
+    }
+}
