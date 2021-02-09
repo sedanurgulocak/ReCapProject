2789e98c20ebaf8b1e5d2546f6236bfca6cc783f
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-09 18:35:32 +0300

    ✨ IDisposable pattern implementation of EfBrandDal

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -11,27 +13,49 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Brand entity)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                var addedEntity = context.Entry(entity); //capture reference
+                addedEntity.State = EntityState.Added; //state what the process is
+                context.SaveChanges(); //execution of the transaction
+            }
         }
 
         public void Delete(Brand entity)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                var deletedEntity = context.Entry(entity); //capture reference
+                deletedEntity.State = EntityState.Deleted; //state what the process is
+                context.SaveChanges(); //execution of the transaction
+            }
         }
 
         public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                return filter == null ? context.Set<Brand>().ToList() : context.Set<Brand>().Where(filter).ToList();
+            }
         }
 
         public Brand GetById(Expression<Func<Brand, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                return context.Set<Brand>().SingleOrDefault(filter);
+
+            }
         }
 
         public void Update(Brand entity)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                var updatedEntity = context.Entry(entity); //capture reference
+                updatedEntity.State = EntityState.Modified; //state what the process is
+                context.SaveChanges(); //execution of the transaction
+            }
         }
     }
 }
