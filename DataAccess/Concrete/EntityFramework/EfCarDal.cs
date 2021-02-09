95b91ccbb30b69af4682a45375a09d96de204d2a
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-10 02:03:36 +0300

    🔨 Write a join operation in the using block for EfCarDal

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -15,7 +15,18 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public List<CarDetailDto> GetCarDetailDtos()
         {
-            
+            using (RentACarContext context= new RentACarContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             join cl in context.Colors
+                             on c.ColorId equals cl.ColorId
+                             select new CarDetailDto { Id = c.Id, BrandName = b.BrandName, 
+                                 ColorName = cl.ColorName, Descriptions = c.Descriptions };
+                return result.ToList();
+
+            }
         }
     }
 }
