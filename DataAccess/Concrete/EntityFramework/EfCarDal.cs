3584b2904435941d2d85b90dd6a8ba76df06c3e6
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-09 18:16:47 +0300

    ✨ IDisposable pattern implementation of EfCarDal

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -11,27 +13,49 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Car entity)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context= new RentACarContext())
+            {
+                var addedEntity = context.Entry(entity); //capture reference
+                addedEntity.State = EntityState.Added; //state what the process is
+                context.SaveChanges(); //execution of the transaction
+            }
         }
 
         public void Delete(Car entity)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                var deletedEntity = context.Entry(entity); //capture reference
+                deletedEntity.State = EntityState.Deleted; //state what the process is
+                context.SaveChanges(); //execution of the transaction
+            }
         }
 
         public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
         }
 
         public Car GetById(Expression<Func<Car, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context=new RentACarContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+
+            }
         }
 
         public void Update(Car entity)
         {
-            throw new NotImplementedException();
+            using (RentACarContext context = new RentACarContext())
+            {
+                var updatedEntity = context.Entry(entity); //capture reference
+                updatedEntity.State = EntityState.Modified; //state what the process is
+                context.SaveChanges(); //execution of the transaction
+            }
         }
     }
 }
