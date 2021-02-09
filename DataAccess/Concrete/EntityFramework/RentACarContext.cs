d397af0831168afa080e77c7e4f00e2e3c7e8ce4
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-09 17:49:22 +0300

    ✨ Add DbContext to associate tables with classes

---
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RentACarContext: DbContext
+    {
+    }
+}
