aa0a4bd53dc857fa06cb3a01060cef5c36cfdece
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-09 17:59:35 +0300

    ✨ Connection RentACar database

--- DataAccess/Concrete/EntityFramework/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class RentACarContext: DbContext
+    public class RentACarContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RentACar;Trusted_Connection=true");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+
+
     }
 }
