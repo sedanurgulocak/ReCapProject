ba73b26c93689add2c5b010183a3f0c1dc227681
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-09 23:17:00 +0300

    ✨Add generic base class for EntityFrameworkDal codes to core layer

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>
+    {
+
+    }
+}
