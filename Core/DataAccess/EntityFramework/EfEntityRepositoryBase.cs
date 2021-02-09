ca67637d722e590b9ed39a19d996a05cea7bae90
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-09 23:35:05 +0300

    ✨ Add expression for EfEntityRepositoryBase

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.DataAccess.EntityFramework
 {
-    public class EfEntityRepositoryBase<TEntity,TContext>
+    public class EfEntityRepositoryBase<TEntity,TContext> 
+        where TEntity:class, IEntity, new()
+        where TContext: DbContext, new()
     {
 
     }
