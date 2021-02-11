f04043b3e630ec4defa0c1960242a830a7304244
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-11 22:59:26 +0300

    ✅ Test CRUD

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -19,6 +20,11 @@ namespace ConsoleUI
         private static void ColorTest()
         {
             ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            //colorManager.Add(new Color {ColorName = "Gri" });
+            //colorManager.Update(new Color {ColorId=1003, ColorName = "Gri" });
+
+
             foreach (var color in colorManager.GetAll())
             {
                 Console.WriteLine(color.ColorName);
@@ -29,6 +35,9 @@ namespace ConsoleUI
         private static void BrandTest()
         {
             BrandManager brandManager = new BrandManager(new EfBrandDal());
+
+            //brandManager.Delete(new Brand {BrandId=4 ,BrandName = "MINI" });
+
             foreach (var brand in brandManager.GetAll())
             {
                 Console.WriteLine(brand.BrandName);
@@ -38,6 +47,11 @@ namespace ConsoleUI
         private static void CarTest()
         {
             CarManager carManager = new CarManager(new EfCarDal());
+
+            //carManager.Add(new Car {BrandId=1, ColorId=1, ModelYear="2021", Descriptions="Manuel dizel", DailyPrice = 500 } );
+
+            //carManager.Update(new Car { Id = 1003, BrandId = 2, ColorId = 3, ModelYear = "1990", Descriptions = "Manuel dizel", DailyPrice = 600});
+           
 
             foreach (var car in carManager.GetCarDetailDtos())
             {
