a2b8128f763b5c135f94998a84f3130751364599
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-11 22:05:31 +0300

    🐛Fix type error, make string decimal

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -11,7 +11,7 @@ namespace Entities.DTOs
         public string Descriptions { get; set; }
         public string BrandName { get; set; }
         public string ColorName { get; set; }
-        public string DailyPrice { get; set; }
+        public decimal DailyPrice { get; set; }
 
     }
 }
