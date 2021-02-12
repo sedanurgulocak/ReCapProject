f2ef647df3892ea74a08db6b1d3ca61b7fbd61cb
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-12 21:00:42 +0300

    ✨ Add new class Result and implement interface

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
