df2f64dad5fc1c8452d2da75ced1e9382748acec
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-12 22:06:39 +0300

    🔨 Add constuructor to Result

--- Core/Utilities/Results/Result.cs
+++ Core/Utilities/Results/Result.cs
@@ -6,6 +6,16 @@ namespace Core.Utilities.Results
 {
     public class Result : IResult
     {
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+
         public bool Success { get; }
 
         public string Message { get; }
