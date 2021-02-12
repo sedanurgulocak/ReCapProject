6e051f345b33b0839abbd099e07362b506aac9c8
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-12 22:56:47 +0300

    ✨ Create Messages static class to get rid of magic strings

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "New car added";
+        public static string CarDeleted = "The car was deleted";
+        public static string CarUpdated = "The car was updated";
+        public static string CarsListed = "Cars listed";
+
+        public static string BrandAdded = "New Brand added";
+        public static string BrandDeleted = "The brand was deleted";
+        public static string BrandUpdated = "The brand was updated";
+        public static string BrandListed = "Brands listed";
+
+        public static string ColorAdded = "New color added";
+        public static string ColorDeleted = "The color was deleted";
+        public static string ColorUpdated = "The color wad updated";
+        public static string ColorListed = "Colors listed";
+    }
+}
