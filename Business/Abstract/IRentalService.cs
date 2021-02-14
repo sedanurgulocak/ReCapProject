da364f96fc7535a8de23fbb1ae4023b883eea1e6
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-15 01:07:34 +0300

    ✨ Add new interface IRentalService

---
+++ Business/Abstract/IRentalService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IRentalService
+    {
+        IDataResult<List<Rental>> GetAll();
+        IDataResult<Rental> GetById(int id);
+        IDataResult<List<RentalDetailDto>> GetRentalDetailsDto(int carId);
+        IResult Add(Rental rental);
+        IResult Delete(Rental rental);
+        IResult Update(Rental rental);
+        IResult CheckReturnDate(int carId);
+        IResult UpdateReturnDate(int carId);
+    }
+}
