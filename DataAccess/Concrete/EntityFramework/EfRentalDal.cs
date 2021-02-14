b7ffd7c7f6abc3f8387d485813739544debe147f
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-14 23:57:04 +0300

    🔨 Add join operations to EfRentalDal

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -1,13 +1,41 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfRentalDal:EfEntityRepositoryBase<Rental, RentACarContext>,IRentalDal
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RentACarContext>, IRentalDal
     {
+        public List<RentalDetailDto> GetRentalDetailDtos()
+        {
+            using (RentACarContext context= new RentACarContext())
+            {
+                var result = from r in context.Rentals
+                             join c in context.Cars
+                             on r.CarId equals c.Id
+                             join cs in context.Customers
+                             on r.CustomerId equals cs.CustomerId
+                             join u in context.Users
+                             on cs.UserId equals u.UserId
+                             select new RentalDetailDto
+                             {
+                                 Id = r.RentalId,
+                                 CarId = c.Id,
+                                 CarDescription = c.Descriptions,
+                                 CompanyName = cs.CompanyName,
+                                 FirstName = u.FirstName,
+                                 LastName= u.LastName,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate
+                             };
+                return result.ToList();
+
+            }
+        }
     }
 }
