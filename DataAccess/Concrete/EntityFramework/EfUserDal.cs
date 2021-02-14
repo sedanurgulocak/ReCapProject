0d14c3f4eefeccc2514de72c1eb1cacbfe207752
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-14 23:03:29 +0300

    ✨ Implement interface to EfUserDal

--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfUserDal
+    public class EfUserDal:EfEntityRepositoryBase<User, RentACarContext>, IUserDal
     {
     }
 }
