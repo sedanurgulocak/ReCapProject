ba0a1763f931d77641cf10fdccdb4f7eb18e5b4c
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-15 00:58:10 +0300

    ✨ Add new class CustomerManager and implement ICustomerService

---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        public IResult Add(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Customer> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
