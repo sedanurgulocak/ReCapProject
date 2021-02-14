158075d7219112feec8c0ecc39381f7146ecf041
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-15 01:09:05 +0300

    ✨ Add new class RentalManager and implement IRentalService

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        public IResult Add(Rental rental)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult CheckReturnDate(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Rental> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<RentalDetailDto>> GetRentalDetailsDto(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Rental rental)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult UpdateReturnDate(int carId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
