2c81d293961e301bba989dc5a810bc9e3dd607f2
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-27 17:45:27 +0300

    ✨ Add new class CarValidator and methods

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Descriptions).NotEmpty();
+            RuleFor(c => c.Descriptions).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+        }
+    }
+}
