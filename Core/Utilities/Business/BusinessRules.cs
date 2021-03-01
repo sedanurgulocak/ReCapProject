06e26c1403e0d1969458eb78f1128cd49cd4f872
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-02 00:20:34 +0300

    🔨Add Run method to BusinessRules

--- Core/Utilities/Business/BusinessRules.cs
+++ Core/Utilities/Business/BusinessRules.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Core.Utilities.Results;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Utilities.Business
 {
-    class BusinessRules
+    public class BusinessRules
     {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+            return null;
+        }
     }
 }
