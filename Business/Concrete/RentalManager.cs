ce6c0f96ff9d17d7d7e39626d8c067bb64a90858
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-01 20:08:35 +0300

    🔨 Add Aspect to Add method for RentalManager

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -20,6 +22,7 @@ namespace Business.Concrete
             _rentalDal = rentalDal;
         }
 
+        [ValidationAspect(typeof(RentalValidator))]
         public IResult Add(Rental rental)
         {
             var result = _rentalDal.GetAll(r=>rental.CarId==rental.CarId && r.ReturnDate==null);
