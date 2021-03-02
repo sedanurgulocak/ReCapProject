b9e73dff9e61dbd9ee75bd5f3cf8b206be6315f5
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-02 20:11:40 +0300

    ✨ Add new class CarImageManager

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager 
+    {
+        ICarImageDal _carImageDal;
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+    }    
+}
