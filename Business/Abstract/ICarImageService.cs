ee5d82918272d542f3b4897bbce175899e6015aa
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-02 20:03:50 +0300

    🔨 Add ICarImageService methods

--- Business/Abstract/ICarImageService.cs
+++ Business/Abstract/ICarImageService.cs
@@ -1,13 +1,21 @@
 ﻿using Core.Utilities.Results;
 using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Business.Abstract
 {
-    public class ICarImageService
+    public interface ICarImageService
     {
-       
+        IResult Add(IFormFile file, CarImage carImage);
+        IResult Update(IFormFile file, CarImage carImage);
+        IResult Delete(CarImage carImage);
+        IDataResult<List<CarImage>> GetAll(Expression<Func<CarImage, bool>> filter = null);
+        IDataResult<CarImage> GetById(int id);
+
+
     }
 }
