d0ab9841b022416289a3b10e6869d3b6af418066
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-09 01:20:41 +0300

    🔨 Add new messages for authorization

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -44,5 +45,12 @@ namespace Business.Constants
         public static string RentalUpdatedReturnDateError = "Vehicle already received";
 
         public static string AuthorizationDenied = "Yetkiniz yok";
+
+        public static string UserRegistered = "Kayıt oldu";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatası";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
     }
 }
