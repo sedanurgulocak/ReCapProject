cc19e0cef6297b77844aa2a645bc374e2da07a35
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-09 16:35:44 +0300

    🔨Call AddDependencyResolvers method at startup and add CoreModule to the list

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -52,7 +54,9 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-            ServiceTool.Create(services);
+            services.AddDependencyResolvers(new ICoreModule[]{
+                new CoreModule()
+            }) ;
 
         }
 
