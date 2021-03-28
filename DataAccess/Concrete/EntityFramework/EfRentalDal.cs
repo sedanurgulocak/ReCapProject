00d2fd5f2443510f5c0b399885a2481f5b7acbd6
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-28 21:52:05 +0300

    🔨Add new method for single car detail to rental dal

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -12,6 +12,7 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRentalDal : EfEntityRepositoryBase<Rental, RentACarContext>, IRentalDal
     {
+
         public List<RentalDetailDto> GetRentalDetailDtos(Expression<Func<Rental, bool>> filter= null)
         {
             using (RentACarContext context= new RentACarContext())
@@ -38,6 +39,35 @@ namespace DataAccess.Concrete.EntityFramework
                                  ReturnDate = r.ReturnDate
                              };
                 return result.ToList();
+
+            }
+        }
+        public RentalDetailDto GetRentalDetail(int id)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from r in context.Rentals.Where(r => r.CarId ==id)
+                             join c in context.Cars
+                             on r.CarId equals c.Id
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             join cs in context.Customers
+                             on r.CustomerId equals cs.CustomerId
+                             join u in context.Users
+                             on cs.UserId equals u.UserId
+                             select new RentalDetailDto
+                             {
+                                 Id = r.RentalId,
+                                 CarId = c.Id,
+                                 CarDescription = c.Descriptions,
+                                 BrandName = b.BrandName,
+                                 CompanyName = cs.CompanyName,
+                                 FirstName = u.FirstName,
+                                 LastName = u.LastName,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate
+                             };
+                return result.SingleOrDefault();
 
             }
         }
