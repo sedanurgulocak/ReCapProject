b7c18019011ad427c0cb1124f75273bbcfcc80d5
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-28 21:40:51 +0300

    🔨Add if for filtering

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -17,40 +17,47 @@ namespace DataAccess.Concrete.EntityFramework
         {
             using (RentACarContext context= new RentACarContext())
             {
-                //var result = from c in context.Cars
-                //             join b in context.Brands
-                //             on c.BrandId equals b.BrandId
-                //             join cl in context.Colors
-                //             on c.ColorId equals cl.ColorId
-                //             join ci in context.CarImages
-                //             on c.Id equals ci.CarId
-                //             select new CarDetailDto { 
-                //                 Id = c.Id, 
-                //                 BrandName = b.BrandName, 
-                //                 ColorName = cl.ColorName, 
-                //                 Descriptions = c.Descriptions, 
-                //                 DailyPrice=c.DailyPrice,
-                //                 CarImageDate=ci.CarImageDate,
-                //                 ImagePath=ci.ImagePath
-
-                //             };
-                //return result.ToList();
-                var result = context.Cars
+                if(filter == null)
+                {
+                    var result = context.Cars
                     .Include(i => i.Brand)
                     .Include(i => i.Color)
                     .Include(i => i.CarImages)
                     .Select(s => new CarDetailDto
+                    {
+                        Id = s.Id,
+                        BrandName = s.Brand.BrandName,
+                        ColorName = s.Color.ColorName,
+                        Descriptions = s.Descriptions,
+                        DailyPrice = s.DailyPrice,
+                        ModelYear = s.ModelYear,
+                        ImagePath = s.CarImages.Select(sk => sk.ImagePath).ToList()
+
+                    }).ToList();
+                    return result;
+                }
+                else
                 {
-                    Id=s.Id,
-                    BrandName=s.Brand.BrandName,
-                    ColorName=s.Color.ColorName,
-                    Descriptions=s.Descriptions,
-                    DailyPrice=s.DailyPrice,
-                    ModelYear=s.ModelYear,
-                    ImagePath=s.CarImages.Select(sk=>sk.ImagePath).ToList()
-                    
-                }).ToList();
-                return result;
+                    var result = context.Cars
+                    .Where(filter)
+                    .Include(i => i.Brand)
+                    .Include(i => i.Color)
+                    .Include(i => i.CarImages)
+                    .Select(s => new CarDetailDto
+                    {
+                        Id = s.Id,
+                        BrandName = s.Brand.BrandName,
+                        ColorName = s.Color.ColorName,
+                        Descriptions = s.Descriptions,
+                        DailyPrice = s.DailyPrice,
+                        ModelYear = s.ModelYear,
+                        ImagePath = s.CarImages.Select(sk => sk.ImagePath).ToList(),
+                        
+
+                    }).ToList();
+                    return result;
+                }
+
             }
         }
 
