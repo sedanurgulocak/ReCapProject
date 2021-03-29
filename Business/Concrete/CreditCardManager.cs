3eaf16f974031fe93d1596691d126ebb6ce6d743
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-29 19:22:27 +0300

    ✨Add new manager for credit card and oparetions

---
+++ Business/Concrete/CreditCardManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CreditCardManager : ICreditCardService
+    {
+        private ICreditCardDal _creditCardDal;
+
+        public CreditCardManager(ICreditCardDal creditCardDal)
+        {
+            _creditCardDal = creditCardDal;
+        }
+
+        public IResult Add(CreditCard creditCard)
+        {
+            _creditCardDal.Add(creditCard);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CreditCard creditCard)
+        {
+            _creditCardDal.Delete(creditCard);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<CreditCard>> GetAll()
+        {
+            return new SuccessDataResult<List<CreditCard>>(_creditCardDal.GetAll());
+        }
+
+        public IDataResult<List<CreditCard>> GetByCardNumber(string cardNumber)
+        {
+            return new SuccessDataResult<List<CreditCard>>(_creditCardDal.GetAll(c => c.CardNumber == cardNumber));
+        }
+
+        public IDataResult<CreditCard> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(CreditCard creditCard)
+        {
+            _creditCardDal.Update(creditCard);
+            return new SuccessResult();
+        }
+    }
+}
