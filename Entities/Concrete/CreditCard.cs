9350928f3ef88296252c36b412c280bebd34a6e2
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-29 18:11:34 +0300

    ✨Add new entity CreditCard

---
+++ Entities/Concrete/CreditCard.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class CreditCard : IEntity
+    {
+        public int id { get; set; }
+        public string NameOnTheCard { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string ExpirationDate { get; set; }
+        public decimal MonetInTheCard { get; set; }
+
+    }
+}
