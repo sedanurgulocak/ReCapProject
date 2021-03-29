677f4ebc40ab13b459e33f8ce14056cd1b3f7eca
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-29 19:21:48 +0300

    ✨Add new interface for credit card service

---
+++ Business/Abstract/ICreditCardService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICreditCardService
+    {
+        IDataResult<List<CreditCard>> GetAll();
+        IDataResult<List<CreditCard>> GetByCardNumber(string cardNumber);
+        IDataResult<CreditCard> GetById(int id);
+        IResult Add(CreditCard creditCard);
+        IResult Delete(CreditCard creditCard);
+        IResult Update(CreditCard creditCard);
+    }
+}
