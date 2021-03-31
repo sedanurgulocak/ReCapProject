4612bb518021c7d818f0af2f6573f0cdaecc62d9
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-31 21:38:08 +0300

    ✨Add pay dto

---
+++ Entities/DTOs/PayDto.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class PayDto : IDto
+    {
+        public int CarId { get; set; }
+        public string NameOnTheCard { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string ExpirationDate { get; set; }
+
+        public DateTime RentDate { get; set; }
+
+        public DateTime ReturnDate { get; set; }
+
+        public decimal AmountPay { get; set; }
+    }
+}
