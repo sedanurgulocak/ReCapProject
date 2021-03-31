5585a7e637570eca18f28e979a9afa288af2ebf0
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-31 21:42:48 +0300

    🔨 Apply payment method to add transaction

---
+++ WebAPI/Controllers/CreditCardsController.cs
@@ -0,0 +1,86 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditCardsController : ControllerBase
+    {
+        private ICreditCardService _creditCardService;
+        public CreditCardsController(ICreditCardService creditCardService)
+        {
+            _creditCardService = creditCardService;
+        }
+        [HttpPost("add")]
+        public IActionResult Add(PayDto pay)
+        {
+            var result = _creditCardService.Pay(pay);
+
+            return Ok(result);
+
+        }
+
+        [HttpPost("delet")]
+        public IActionResult Delete(CreditCard creditCard)
+        {
+            var result = _creditCardService.Delete(creditCard);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(CreditCard creditCard)
+        {
+            var result = _creditCardService.Update(creditCard);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _creditCardService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _creditCardService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("getbycardnumber")]
+        public IActionResult GetByCardNumber(String cardNumber)
+        {
+            var result = _creditCardService.GetByCardNumber(cardNumber);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
