13f8f93b52b333709557dff255e1aec8a6108fb2
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-09 01:02:39 +0300

    🔨Add new method get by email and Add new parameter user password for update method

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -40,6 +40,24 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
+        [HttpGet("getbyemail")]
+        public IActionResult GetByEmail(string email)
+        {
+            var result = _userService.GetByMail(email);
+            if (result.Success)
+            {
+                return Ok(new
+                {
+                    result.Data.UserId,
+                    result.Data.FirstName,
+                    result.Data.LastName,
+                    result.Data.Email,
+                    result.Data.Status
+                });
+            }
+            return BadRequest(result);
+        }
+
         [HttpPost("add")]
         public IActionResult Add(User user)
         {
@@ -63,9 +81,10 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost("update")]
-        public IActionResult Update(User user)
+        public IActionResult Update([FromBody]User user, [FromQuery]string password)
         {
-            var result = _userService.Update(user);
+
+            var result = _userService.Update(user, password);
             if (result.Success)
             {
                 return Ok(result);
